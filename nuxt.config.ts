fe2f0db8b740f5f4331d8f27cb1b3430848afbe9
Rick Schultz <rick-schultz@hotmail.com>
2025-10-19 15:06:11 -0300

    feat: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
