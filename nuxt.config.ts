cb75abecc0626bd3a61bd9cbdf03f0eb7cabcb01
Rick Schultz <rick-schultz@hotmail.com>
2025-10-19 13:33:35 -0300

    feat: add nuxt plugins for state and date management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/test-utils',
+    'dayjs-nuxt'
+  ]
 })
